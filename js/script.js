aca2794734b4d8a2ec59a7ea3559ece1e0aa45ac
Junyu Wang <wjyselina@hotmail.com>
2022-07-10 20:45:07 -0700

    added 3 pokemon objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favFood = "breakfast_burrito"
-document.write(favFood)
+let pokemonList = [
+    Nidoran = {
+        name: 'Nidoran',
+        category: [poison, pin],
+        weight: 7
+    },
+    Cherubi = {
+        name: 'Cherubi',
+        category: [fairy, grass],
+        weight: 3.3
+    },
+    Cubone = {
+        name: 'Cubone',
+        category: [monster],
+        weight: 6.5
+    }
+]
