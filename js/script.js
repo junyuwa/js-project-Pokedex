66d5c4e59d4266024c28dc8fc7e282374007c950
Junyu Wang <wjyselina@hotmail.com>
2022-07-15 00:13:37 -0700

    used forEach to iterate, put global variables in IIFE, added validation for new input

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
-let pokemonList = [
-    {
-        name: 'Nidoran',
-        category: ["poison", "pin"],
-        weight: 7,
-        height: 0.5
-    },
-    {
-        name: 'Cherubi',
-        category: ["fairy", "grass"],
-        weight: 3.3,
-        height: 0.4
-    },
-    {
-        name: 'Cubone',
-        category: ["monster"],
-        weight: 6.5,
-        height: 0.4
+const pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Nidoran',
+            category: ["poison", "pin"],
+            weight: 7,
+            height: 0.5
+        },
+        {
+            name: 'Cherubi',
+            category: ["fairy", "grass"],
+            weight: 3.3,
+            height: 0.4
+        },
+        {
+            name: 'Cubone',
+            category: ["monster"],
+            weight: 6.5,
+            height: 0.4
+        }
+    ];
+    let getAll = function () {
+        return pokemonList;
     }
-]
+    let add = function (item) {
+        if (typeof item === obejct && Object.keys === [name, category, weight, height]) {
+            pokemonList.push(item);
+        } else {
+            alert('Please enter the right pokemon info');
+        }
+    };
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height <= 0.4) {
-        document.write(`${pokemonList[i].name}` + `(${pokemonList[i].height})` + "-a small pokemon" + " ");
-    } else {
-        document.write(`${pokemonList[i].name}` + `(${pokemonList[i].height})` + " ");
+    return {
+        getAll,
+        add
     }
-}
+})
+
+
+function printPokemonList(pokemon) {
+    document.write(pokemon.name + '' + pokemon.height)
+};
+
+getAll.forEach(printPokemonList);
