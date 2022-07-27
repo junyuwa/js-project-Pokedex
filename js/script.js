0b09170bb0b208ce4e4dc536c4e47c6e9c0530d5
Junyu Wang <wjyselina@hotmail.com>
2022-07-26 21:41:57 -0700

    tried to add show modal function, to be further improved

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const pokemonRepository = (function () {
     let pokemonInfoList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let modalContainer = document.querySelector('#modal-container');
 
     let add = function (pokemon) {
         if (typeof pokemon === 'object' && 'name' in pokemon && 'detailsUrl' in pokemon) {
@@ -10,9 +11,9 @@ const pokemonRepository = (function () {
         };
     };
 
-    function showDetails(pokemon) {
-        console.log(pokemon.name);
-    }
+    // function showDetails(pokemon) {
+    //     console.log(pokemon.name);
+    // }
 
     let getAll = function () {
         return pokemonInfoList;
@@ -24,12 +25,12 @@ const pokemonRepository = (function () {
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
-        button.classList.add('pokemonName');
+        button.classList.add('pokemonName', 'show-modal');
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
         // click on button to show details in console
         button.addEventListener('click', function (event) {
-            showDetails(pokemon)
+            showModal(pokemon);
         });
     };
 
@@ -57,23 +58,56 @@ const pokemonRepository = (function () {
             //add the details to the item
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
-            item.types = details.types;
+            item.name = details.name;
         }).catch(function (e) {
             console.error(e);
         });
     }
 
-    function showDetails(pokemon) {
+    function showModal(pokemon) {
+
         loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+            // clears the elements created otehrwise they will appear multiple times with multiple clicks
+            modalContainer.innerHTML = '';
         });
+
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = `${pokemon.name}`;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = `Height:${pokemon.height}`;
+
+        let imageElement = document.createElement('p');
+        imageElement.innerHTML = `<img src=${pokemon.imageUrl} alt=${pokemon.name}>`;
+
+        let closeButton = document.createElement('button');
+        closeButton.classList.add('modal-close');
+        closeButton.innerText = 'Close';
+        closeButton.addEventListener('click', hideModal);
+
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(imageElement);
+        modal.appendChild(closeButton);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
     }
+
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    };
 
     return {
         getAll: getAll,
         add: add,
         addListItem: addListItem,
-        showDetails: showDetails,
+        showModal: showModal,
+        hideModal: hideModal,
         loadList: loadList,
         loadDetails: loadDetails
     }
@@ -85,3 +119,13 @@ pokemonRepository.loadList().then(function () {
         pokemonRepository.addListItem(pokemon);
     });
 });
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
+    }
+});
+
+// document.querySelector('.show-modal').addEventListener('click', () => {
+//     showModal(pokemon);
+// });
